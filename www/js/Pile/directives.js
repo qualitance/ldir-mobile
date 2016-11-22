ff086423d5cc145000a9b3522c0a2f3930684b96
Gabriel Muscalu <gabriel.muscalu@qualitance.com>
2016-11-22 16:47:54 +0200

    documented profile module

--- www/js/Pile/directives.js
+++ www/js/Pile/directives.js
@@ -1,8 +1,11 @@
-/**
- * Created by cristian on 27/4/2015.
- */
-
 angular.module('PileModule')
+    /**
+     * @ngdoc directive
+     * @name reportProgress
+     * @description report progress in pile details view, clickable after user reaches last step
+     * @example
+     * <pre><report-progress step="{{pile.progressStep}}" clickable="pile.hasReachedLastStep"></report-progress></pre>
+     */
     .directive('reportProgress', ['$state', function ($state) {
         return {
             restrict: 'E',
@@ -18,6 +21,13 @@ angular.module('PileModule')
         };
     }])
 
+    /**
+     * @ngdoc directive
+     * @name pileSizeCard
+     * @description pile size card directive, editable if reporting is in step4
+     * @example
+     * <pre><pile-size-card size="pile.size" title="Size" editable="1"></pile-size-card></pre>
+     */
     .directive('pileSizeCard', function () {
         return {
             restrict: 'E',
@@ -34,6 +44,13 @@ angular.module('PileModule')
         };
     })
 
+    /**
+     * @ngdoc directive
+     * @name pileSize
+     * @description pile size directive
+     * @example
+     * <pre><pile-size class="popup-size" editable="false" size="pile.size"></pile-size></pre>
+     */
     .directive('pileSize', function () {
         return {
             restrict: 'E',
@@ -50,6 +67,13 @@ angular.module('PileModule')
         };
     })
 
+    /**
+     * @ngdoc directive
+     * @name pileContents
+     * @description pile contents select directive
+     * @example
+     * <pre><pile-contents contents="pile.contents"></pile-contents></pre>
+     */
     .directive('pileContents', function () {
         return {
             restrict: 'E',
@@ -60,6 +84,13 @@ angular.module('PileModule')
         };
     })
 
+    /**
+     * @ngdoc directive
+     * @name pileAreas
+     * @description pile contents select directive
+     * @example
+     * <pre><pile-areas areas="pile.areas"></pile-areas></pre>
+     */
     .directive('pileAreas', function () {
         return {
             restrict: 'E',
@@ -79,6 +110,13 @@ angular.module('PileModule')
         };
     })
 
+    /**
+     * @ngdoc directive
+     * @name pilePhoto
+     * @description pile single photo directive
+     * @example
+     * <pre><pile-photo photo="photo" ng-click="zoomImage(photo.displaySrc)" remove-photo="removePhoto($index)" has-controls="true"></pile-photo></pre>
+     */
     .directive('pilePhoto', function ($timeout) {
         return {
             restrict: 'E',
@@ -100,6 +138,13 @@ angular.module('PileModule')
         };
     })
 
+    /**
+     * @ngdoc directive
+     * @name helpStep
+     * @description help steps images directive
+     * @example
+     * <pre><help-step step="{{i}}" language="{{language}}"></help-step></pre>
+     */
     .directive('helpStep', function () {
         return {
             restrict: 'E',
